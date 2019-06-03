0f835d5dc8594da1ce48036e2739df593adc1037
StrandGear <strandgear@mail.ru>
2019-06-03 17:31:01 +0300

    JavaScript elements were added

---
+++ src/js/main.js
@@ -0,0 +1,13 @@
+var button = document.querySelector('#button'); //ищем внутри документа html,querySelector позволяет найти определенный элемент
+var modal = document.querySelector('#modal');
+var close = document.querySelector('#close');
+
+button.addEventListener('click', function() {
+   modal.classList.add('modal_active');
+   
+});
+
+close.addEventListener('click', function() {
+   modal.classList.remove('modal_active');
+   
+})
